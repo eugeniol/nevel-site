25ebe6991f552f00902859117b6a2863ff1d5f88
Eugenio Lattanzio <eugenio63@gmail.com>
2016-10-18 14:40:53 -0300

    Add passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,10 @@
+import sys, os
+
+INTERP = "/home/euglat1/python-django-2.7/bin/python"
+# INTERP is present twice so that the new Python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
 import nevelsite.wsgi
+
 application = nevelsite.wsgi.application
 
